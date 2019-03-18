bb72b68906011dbe0c16b9aaa2952182ec7f1802
thinking <thinking_fioa@163.com>
2019-03-18 19:50:15 +0800

    chapter5 list11

--- goinaction/src/chapter5/listing11/listing11.go
+++ goinaction/src/chapter5/listing11/listing11.go
@@ -1,14 +1,30 @@
 package main
 
+import "fmt"
+
 type user struct {
 	name  string
 	email string
 }
 
 func (u user) notify() {
-	// TODO ...
+	fmt.Printf("name %s, email %s.", u.name, u.email)
+	fmt.Println()
+}
+
+func (u *user) changeEmail(email string) {
+	u.email = email
 }
 
 func main() {
+	u := user{"thinking", "thinking_fioa@163.com"}
 
+	u.notify()
+	u.changeEmail("thinkingfioa@163.com")
+	u.notify()
+
+	uPoint := &user{"triple3P", "triple3P@163.com"}
+	uPoint.notify()
+	uPoint.changeEmail("sb@163.com")
+	uPoint.notify()
 }
